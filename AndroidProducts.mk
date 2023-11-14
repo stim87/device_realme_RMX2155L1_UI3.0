8aa8f5242f462e08d216390d3d77944019ee8be2
stim <141650876+stim87@users.noreply.github.com>
2023-11-15 00:20:05 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,14 @@
+#
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
 # SPDX-License-Identifier: Apache-2.0
+#
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_RMX2155.mk
+    $(LOCAL_DIR)/twrp_ossi.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_RMX2155-user \
-    twrp_RMX2155-userdebug \
-    twrp_RMX2155-eng
+    twrp_ossi-user \
+    twrp_ossi-userdebug \
+    twrp_ossi-eng
