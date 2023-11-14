a9f6213d75ed95368c93a8d1915b967bf741f663
stim <141650876+stim87@users.noreply.github.com>
2023-11-15 03:25:26 +0500

    Update twrp_ossi.mk

--- twrp_ossi.mk
+++ twrp_ossi.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/realme/ossi/device.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit from TWRP-common stuff, if building TWRP.
