86a5bdfe11658cd532b95ac74c9e3825c93852c4
Stim <141650876+stim87@users.noreply.github.com>
2023-11-20 23:55:08 +0500

    Update and rename twrp_ossi.mk to twrp_RMX2155L1.mk

--- twrp_RMX2155L1.mk
+++ twrp_RMX2155L1.mk
@@ -6,7 +6,7 @@
 #
 
 # Inherit from RMX2155 device
-$(call inherit-product, device/realme/ossi/device.mk)
+$(call inherit-product, device/realme/RMX2155L1/device.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -22,18 +22,11 @@ $(call inherit-product-if-exists, vendor/twrp/config/common.mk)
 # Inherit from PBRP-common stuff, if building PBRP.
 $(call inherit-product-if-exists, vendor/pb/config/common.mk)
 
-PRODUCT_DEVICE := ossi
+PRODUCT_DEVICE := RMX2155L1
 PRODUCT_NAME := twrp_$(PRODUCT_DEVICE)
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := RMX2155
+PRODUCT_MODEL := realme 7
 PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
-PRODUCT_RELEASE_NAME := realme 7
+PRODUCT_RELEASE_NAME := RMX2155
 
 PRODUCT_GMS_CLIENTID_BASE := android-$(PRODUCT_MANUFCATURER)
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=RMX2155EEA \
-    TARGET_DEVICE=RMX2155L1 \
-    PRIVATE_BUILD_DESC="sys_mssi_64_cn_armv82-user 12 SP1A.210812.016 1668670161293 release-keys"
-
-BUILD_FINGERPRINT := realme/RMX2155EEA/RMX2155L1:12/SP1A.210812.016/Q.GDPR.bf75e7-1:user/release-keys
