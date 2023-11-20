8145b3ae67c3a957f0cd8f6e006a6d5c2c0db7c7
Stim <141650876+stim87@users.noreply.github.com>
2023-11-20 23:12:45 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -18,13 +18,6 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.fuse.passthrough.enable=true
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
-BOARD_SHIPPING_API_LEVEL := 30
-
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
 
 # fastbootd
 PRODUCT_PACKAGES += \
